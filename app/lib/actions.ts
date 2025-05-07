76ffc0c82a8ac93fb3f8c63b30e7f34e20ad4f6c
vi1etta <violet.samko@gmail.com>
2025-05-07 17:08:48 +0300

    feat: add page to catch errors and display fallback to user

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -18,6 +18,7 @@ const FormSchema = z.object({
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
+
     const { customerId, amount, status } = CreateInvoice.parse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
@@ -25,13 +26,18 @@ export async function createInvoice(formData: FormData) {
     });
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
- 
+    try {
     await sql`
       INSERT INTO invoices (customer_id, amount, status, date)
       VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
+  } catch (error) {
+    // We'll log the error to the console for now
+    console.error(error);
+  }
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
+
   }
 
   // Use Zod to update the expected types
@@ -47,13 +53,16 @@ export async function updateInvoice(id: string, formData: FormData) {
   });
  
   const amountInCents = amount * 100;
- 
+  try {
   await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `;
- 
+} catch (error) {
+  // We'll log the error to the console for now
+  console.error(error);
+}
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
