9576ce06160ad61a2eecbb6bb07282e2e366deab
vi1etta <violet.samko@gmail.com>
2025-05-09 18:44:57 +0300

    chore: disable linting during prod builds

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,12 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   experimental: {
     ppr: 'incremental'
-  }
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
